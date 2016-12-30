22cf0d10c30420ec71dab378ad146256e0641859
Andrew Walden <awalden0808@gmail.com>
2016-12-30 11:54:03 -0600

    Bean moj should be generated now

--- code.js
+++ code.js
@@ -1,3 +1,25 @@
 function getMoj(i, j) {
+  return ":" + i.toString() + "-" + j.toString() + ":";
+}
 
+function getMojis(m, n) {
+  var mojes = "";
+  for (var i = 0; i < m; i++) {
+    for (var j = 0; j < n; j++) {
+      mojes += getMoj(i, j);
+    }
+    mojes += "\n";
+  }
+  return mojes;
+}
+
+function getBean() {
+  return getMojis(9, 7);
+}
+
+window.onload = function () {
+  var textarea = document.createElement("textarea");
+  textarea.readonly = true;
+  textarea.innerHTML = getBean();
+  document.body.appendChild(textarea);
 }
